eb65aee82d075262712b610b2232da907ad5d9a3
Qedrus <josmartos@me.com>
2024-09-07 00:08:55 +0200

    fix main.py

--- api/main.py
+++ api/main.py
@@ -4,9 +4,10 @@ import qrcode
 import boto3
 import os
 from io import BytesIO
-
-# Loading Environment variable (AWS Access Key and Secret Key)
+from urllib.parse import quote_plus, urlparse
 from dotenv import load_dotenv
+
+# Load environment variables
 load_dotenv()
 
 app = FastAPI()
@@ -24,12 +25,33 @@ app.add_middleware(
 )
 
 # AWS S3 Configuration
+aws_access_key = os.getenv("AWS_ACCESS_KEY")
+aws_secret_key = os.getenv("AWS_SECRET_KEY")
+
+# Check if AWS credentials are loaded properly
+if not aws_access_key or not aws_secret_key:
+    raise HTTPException(status_code=500, detail="AWS credentials are not set properly")
+
 s3 = boto3.client(
     's3',
-    aws_access_key_id= os.getenv("AWS_ACCESS_KEY"),
-    aws_secret_access_key= os.getenv("AWS_SECRET_KEY"))
+    aws_access_key_id=aws_access_key,
+    aws_secret_access_key=aws_secret_key
+)
 
-bucket_name = 'YOUR_BUCKET_NAME' # Add your bucket name here
+bucket_name = "qrcode-storage-qedrus-1"
+test_data = b'This is a test'  # Example binary data
+
+try:
+    s3.put_object(
+        Bucket=bucket_name, 
+        Key="test-upload/test.txt", 
+        Body=test_data, 
+        ContentType='text/plain', 
+        ACL='public-read'
+    )
+    print(f"File uploaded successfully.")
+except Exception as e:
+    print(f"Error: {e}")
 
 @app.post("/generate-qr/")
 async def generate_qr(url: str):
@@ -50,16 +72,26 @@ async def generate_qr(url: str):
     img.save(img_byte_arr, format='PNG')
     img_byte_arr.seek(0)
 
-    # Generate file name for S3
-    file_name = f"qr_codes/{url.split('//')[-1]}.png"
+    # Generate sanitized file name for S3
+    parsed_url = urlparse(url)
+    file_name = f"qr_codes/{quote_plus(parsed_url.netloc + parsed_url.path)}.png"
 
     try:
         # Upload to S3
-        s3.put_object(Bucket=bucket_name, Key=file_name, Body=img_byte_arr, ContentType='image/png', ACL='public-read')
+        s3.put_object(
+            Bucket=bucket_name, 
+            Key=file_name, 
+            Body=img_byte_arr, 
+            ContentType='image/png', 
+            #ACL='public-read'
+        )
         
-        # Generate the S3 URL
+         # Generate the S3 URL
         s3_url = f"https://{bucket_name}.s3.amazonaws.com/{file_name}"
         return {"qr_code_url": s3_url}
+    except boto3.exceptions.S3UploadFailedError as e:
+        print(f"Upload failed: {e}")
+        raise HTTPException(status_code=500, detail=f"Upload failed: {str(e)}")
     except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
-    
+        print(f"An unexpected error occurred: {e}")
+        raise HTTPException(status_code=500, detail=f"An unexpected error occurred: {str(e)}")
